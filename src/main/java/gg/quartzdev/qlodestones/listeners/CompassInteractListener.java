812d00229e828e908d0a4e8c4ecaf8e2a4ef2ba5
QarthO <qartho@gmail.com>
2023-12-31 15:41:56 -0500

    began compass interact listener structure

--- src/main/java/gg/quartzdev/qlodestones/listeners/CompassInteractListener.java
+++ src/main/java/gg/quartzdev/qlodestones/listeners/CompassInteractListener.java
@@ -5,10 +5,19 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class CompassInteractListener implements Listener{
+
+
+    public void test(InventoryClickEvent event){
+        Player player = (Player) event.getWhoClicked();
+        int slot = event.getSlot();
+    }
+
+
 
     @EventHandler
     public void onCompassInteract(PlayerInteractEvent event){
@@ -22,11 +31,12 @@ public class CompassInteractListener implements Listener{
             return;
         }
 
-        Block block = event.getClickedBlock();
-
-        if(block == null || block.getType() != Material.LODESTONE){
+        Block lodestone = event.getClickedBlock();
+        if(lodestone == null || lodestone.getType() != Material.LODESTONE){
             this.openCompassUI(player);
         }
+
+
 
     }
 
